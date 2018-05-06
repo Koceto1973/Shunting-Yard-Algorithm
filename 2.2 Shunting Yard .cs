d5ee322f276ff76264db8970d25e7f4e8bca06fc
Team K <36779077+TeamK@users.noreply.github.com>
2018-05-06 19:15:47 +0300

    operators dictionary fix

--- 2.2 Shunting Yard .cs
+++ 2.2 Shunting Yard .cs
@@ -5,37 +5,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
- 
+
 public class Program
 {
-    public static void Main() 
+    public static void Main()
     {
         string infix = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
         Console.WriteLine(infix.ToPostfix());
     }
 }
- 
-// static class - string extension method and it's data collector
+
 public static class ShuntingYard
-{ // ToDictionary(key selector function) extension method applied over new anonymous type array
-    private static readonly var
-        operators = new (string symbol, int precedence, bool rightAssociative) [] // anonymous type array here
-            {   ("^", 4, true), 
-                ("*", 3, false),  
-                ("/", 3, false),  
-                ("+", 2, false),    
-                ("-", 2, false)
-            }.ToDictionary(op => op.symbol); // https://msdn.microsoft.com/en-us/library/bb549277(v=vs.110).aspx
-            
-    // finally dictionary "operators" is build with key=symbol and value=symbol,precedence,rightAssociative
- 
-    // custom extension method of string class
+{
+	static readonly var	operators = new[]  {
+		new { symbol="^", precedence=4, rightAssociative=true },
+		new { symbol="*", precedence=3, rightAssociative=false},
+		new { symbol="/", precedence=3, rightAssociative=false},
+		new { symbol="+", precedence=2, rightAssociative=false},
+		new { symbol="-", precedence=2, rightAssociative=false} }.ToDictionary(op => op.symbol);
+
+
     public static string ToPostfix(this string infix)
     {
         string[] tokens = infix.Split(' '); // tokens separated
         var stack = new Stack<string>();    // stack is ready
         var output = new List<string>();    // list is ready
-        
+
         // tokens processing
         foreach (string token in tokens) // while there are tokens to be read:
         {
@@ -43,33 +38,33 @@ public static class ShuntingYard
             {
                 output.Add(token);  //  push it to the output queue.
                 Print(token); // ????
-            } 
-            else if (operators.TryGetValue(token, out var op1)) 
+            }
+            else if (operators.TryGetValue(token, out var op1))
             {
                 while (stack.Count > 0 && operators.TryGetValue(stack.Peek(), out var op2))
                 {
                     int c = op1.precedence.CompareTo(op2.precedence);
-                    if (c < 0 || !op1.rightAssociative && c <= 0) 
+                    if (c < 0 || !op1.rightAssociative && c <= 0)
                     {
                         output.Add(stack.Pop());
-                    } 
-                    else 
+                    }
+                    else
                     {
                         break;
                     }
                 }
                 stack.Push(token);
                 Print(token);
-            } 
-            else if (token == "(") 
+            }
+            else if (token == "(")
             {
                 stack.Push(token);
                 Print(token);
-            } 
+            }
             else if (token == ")")
             {
                 string top = "";
-                while (stack.Count > 0 && (top = stack.Pop()) != "(") 
+                while (stack.Count > 0 && (top = stack.Pop()) != "(")
                 {
                     output.Add(top);
                 }
@@ -77,19 +72,19 @@ public static class ShuntingYard
                 Print(token);
             }
         }
-        
+
         while (stack.Count > 0)
         {
             var top = stack.Pop();
             if (!operators.ContainsKey(top)) throw new ArgumentException("No matching right parenthesis.");
             output.Add(top);
         }
-        
+
         Print("pop");
         return string.Join(" ", output);
- 
+
         //Yikes!
-        void Print(string action) => 
+        void Print(string action) =>
             Console.WriteLine($"{action + ":",-4} {$"stack[ {string.Join(" ", stack.Reverse())} ]",-18} {$"out[ {string.Join(" ", output)} ]"}");
         //A little more readable?
         void Print(string action) =>
